bbbc41a3b4971afcb1e46f84b38a4f3a12ba7ea1
Steven Ruppert <blendmaster@gmail.com>
2011-11-01 22:38:16 -0600

    working index.php

--- index.php
+++ index.php
@@ -16,13 +16,6 @@ shuffle($pictures);
 <img src="images/<?php echo $pictures[1] ?>" />
 <img src="images/<?php echo $pictures[2] ?>" />
 
-<?php
-	foreach(get_products() as $name => $product) {
-		$price = $product['price'];
-		echo "<p>$name: $$price</p>";
-	}
-?>
-
 <form action="order.php" method='post'>
 	<label for="name">Your name: </label> <input type="text" name='name' id='name' />
 	<table>
@@ -32,28 +25,22 @@ shuffle($pictures);
 			<td>Quantity</td>
 			<td>Type</td>
 		</tr>
-		<?php
+<?php
 			foreach(get_products() as $name => $product) {
-				echo "<tr>";
-				echo "	<td>$name</td>";
-				echo sprintf("	<td>$%s</td>", $product['price']);
-				echo "<td><input type='number' name='$name' id='$name' value='0' "
-				echo "</tr>";
+				echo "		<tr>\n";
+				echo "			<td>$name</td>\n";
+				echo sprintf("			<td>$%s</td>\n", $product['price']);
+				echo "			<td><input type='number' name='$name' id='$name' value='0' min='0' /></td>\n";
+				if( count($product['types']) > 0 ) {
+					echo "			<td>\n				<select name='$name-type' id='$name-type'>\n";
+					foreach($product['types'] as $type) {
+						echo "					<option value='$type'>$type</option>\n";
+					}
+					echo "				</select>\n			</td>\n";
+				}
+				echo "		</tr>\n";
 			}
-		?>
-		<tr>
-			<td>Thingamabobs</td>
-			<td>$</td>
-			<td>
-				<input type="number" name='thingamabobs' id="thingamabobs" value='0' min='0' />
-			</td>
-			<td>
-			<select name="thingtype" id="thingtype">
-				<option value="gadget">Gadget</option>
-				<option value="gizmo">Gizmo</option>
-			</select>
-			</td>
-		</tr>
+?>
 	</table>
 	<input type="reset" /><input type="submit" />
 </form>
